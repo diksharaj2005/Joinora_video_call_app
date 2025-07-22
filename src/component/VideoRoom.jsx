3e0d633d125fb471f76d727e133d1c52ad29d916
Diksha Raj <dikshanraj25@gmail.com>
2025-07-23 01:21:23 +0530

    Update VideoRoom.jsx

--- src/component/VideoRoom.jsx
+++ src/component/VideoRoom.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react'
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt'
+import { useParams } from 'react-router-dom'
+
 const VideoRoom = () => {
+  const { id } = useParams()
+  const containerRef = useRef(null)
 
-    const {id}=useParams()
+  useEffect(() => {
+    const appID = 1671517269
+    const serverSecret = '984ec4eda1581ba50f1e246d41cd30d6'
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      id,
+      Date.now().toString(),
+      'diksha'
+    )
 
-const meeting=(element)=>{
-     const appID =1671517269 ;
-      const serverSecret = "984ec4eda1581ba50f1e246d41cd30d6";
-      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, id ,  Date.now().toString(),  "diksha");
+    const zp = ZegoUIKitPrebuilt.create(kitToken)
 
-         // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-
-      zp.joinRoom({
-        container:element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:`https://joinora.netlify.app/room/${id}`
-          },
-        ],
-         scenario: {
-          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+    zp.joinRoom({
+      container: containerRef.current,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url: `https://joinora.netlify.app/room/${id}`,
         },
-      })
-}
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    })
+  }, [id])
 
-
-  return (
-    <>
- <div ref={meeting}/>
-    </>
-  )
+  return <div ref={containerRef} style={{ width: '100vw', height: '100vh' }} />
 }
 
 export default VideoRoom
